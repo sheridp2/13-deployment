1a18a5c6946082df0033809d3fbd6925598aa077
Patrick Sheridan <sheridp2@gmail.com>
2017-03-30 12:42:36 -0700

    deploy

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -7,7 +7,7 @@
 
   repos.requestRepos = function(callback) {
     // DONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route. Since the new proxy route is handling our token for us, we do not need it here, which is why we can refactor to to the simpler $.get method.
-    $.get('/github/user/repos?type=owner')
+    $.get('/api.github/user/repos?owner')
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
   };
